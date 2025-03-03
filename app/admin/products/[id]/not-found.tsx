f6ddf55c07cc90cd9d970831d908ad877e9c0abb
wily ramos <wily.ramos1@unmsm.edu.pe>
2025-03-03 11:28:36 -0500

    fix deploy

--- app/admin/products/[id]/not-found.tsx
+++ app/admin/products/[id]/not-found.tsx
@@ -7,7 +7,7 @@ export default function notFound() {
         <div className='text-center '>
             <Heading>Producto no encontrado</Heading>
 
-            <p className='text-gray-500 text-xl'>
+            <p className='text-gray-500 text-2xl'>
                 Tal vez quieres vovler a
                 <Link className='text-blue-500 font-bold ' href='/admin/products?page=1'> productos
                 </Link>
